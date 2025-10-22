7bf116c56d6eaeeb8617150ad09ea9a06462361e
240541125-jpg <240541125@firat.edu.tr>
2025-10-22 11:24:44 +0300

    Update UcMesaj.java

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -1,6 +1,8 @@
-   public static void main(String[] args) {
+   public class UcMesaj {
+      public static void main(String[] args) {
         System.out.println("Javaya Hoş Geldiniz");
         System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
         System.out.println("Programlama Eğlencelidir");
     }
 }
+

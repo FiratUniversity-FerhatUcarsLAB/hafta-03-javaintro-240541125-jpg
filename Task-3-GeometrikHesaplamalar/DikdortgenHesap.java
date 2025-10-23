73cb015dd5629f7d50f9c12373c9b23aa26e0ea5
240541125-jpg <240541125@firat.edu.tr>
2025-10-23 12:29:12 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+ public class DikdörtgenHesap {
+    public static void main (String[] args) {
+   double en = 4.5;
+   double boy = 7.9;
+   double alan = en*boy;
+   double çevre = 2*(en+boy);
+   System.out.println("Dikdörtgenin eni:" + en);
+   System.out.println("Dikdörtgenin boyu:" +boy);
+   System.out.println("Alan:" +alan);
+   System.out.println("Çevre:" +çevre);
+    }
+}

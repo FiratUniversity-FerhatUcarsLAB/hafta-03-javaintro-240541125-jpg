2ff3957e7192c5c79e83ee95e5a3ca8d06857ff2
240541125-jpg <240541125@firat.edu.tr>
2025-10-23 12:32:17 +0300

    Update UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,19 @@
+public class UcgenAlanHesaplama {
+    public static void main(String[] args) {
+        
+        double a = 3;
+        double b = 4;
+        double c = 5;
+
+        
+        double s = (a + b + c) / 2;
+
+        
+        double alan = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+        
+        System.out.println("Üçgenin kenarları: a=" + a + ", b=" + b + ", c=" + c);
+        System.out.println("Yarı çevre (s): " + s);
+        System.out.println("Alan: " + alan);
+    }
+}
